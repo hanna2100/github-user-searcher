fd11832243379351aad805c9224592f6ade5b8c7
장한나림 <janghannarim@janghannalim-ui-MacBookPro.local>
2022-03-06 03:34:12 +0900

    user list ripple 및 애니메이션효과 추가

--- app/src/main/java/com/example/githubusersearch/framework/presentation/search/composable/UserListView.kt
+++ app/src/main/java/com/example/githubusersearch/framework/presentation/search/composable/UserListView.kt
@@ -4,6 +4,7 @@ import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.core.*
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
@@ -15,13 +16,15 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.getValue
+import androidx.compose.material.ripple.LocalRippleTheme
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
@@ -33,7 +36,9 @@ import coil.request.ImageRequest
 import com.example.githubusersearch.R
 import com.example.githubusersearch.business.domain.model.User
 import com.example.githubusersearch.common.extensions.OnBottomReached
+import com.example.githubusersearch.framework.presentation.theme.DarkRippleTheme
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun UserListView(
     users: List<User>,
@@ -41,51 +46,52 @@ fun UserListView(
     onBottomReached: ()->Unit
 ) {
     val listState = rememberLazyListState()
-
-    Box(modifier = Modifier.fillMaxWidth()) {
-        LazyColumn(
-            state = listState,
-        ) {
-            itemsIndexed(items = users) { index, item ->
-                UserCard(
-                    imageUrl = item.defaultInfo.avatarUrl,
-                    login = item.defaultInfo.login
-                )
-            }
-        }
-        AnimatedVisibility(
-            visible = loading,
-            enter = fadeIn(
-                animationSpec = tween(
-                    durationMillis = 200,
-                )
-            ),
-            exit = fadeOut(
-                animationSpec = tween(
-                    durationMillis = 200,
-                )
-            )
-        ) {
-            Column(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .background(Color.Black.copy(alpha = 0.3f)),
-                horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.Center
+    CompositionLocalProvider(LocalRippleTheme provides DarkRippleTheme) {
+        Box(modifier = Modifier.fillMaxWidth()) {
+            LazyColumn(
+                state = listState,
             ) {
-                DotsTyping(
-                    dotColor = Color.White,
-                    dotSize = 14.dp,
-                    delayUnit = 300
-                )
-                Spacer(Modifier.size(10.dp))
-                Text(
-                    text = "Loading",
-                    style = MaterialTheme.typography.h5.copy(
-                        color = Color.White,
-                        fontWeight = FontWeight.Medium
+                itemsIndexed(items = users) { index, item ->
+                    UserCard(
+                        imageUrl = item.defaultInfo.avatarUrl,
+                        login = item.defaultInfo.login
+                    )
+                }
+            }
+            AnimatedVisibility(
+                visible = loading,
+                enter = fadeIn(
+                    animationSpec = tween(
+                        durationMillis = 200,
+                    )
+                ),
+                exit = fadeOut(
+                    animationSpec = tween(
+                        durationMillis = 200,
                     )
                 )
+            ) {
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(Color.Black.copy(alpha = 0.3f)),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center
+                ) {
+                    DotsTyping(
+                        dotColor = Color.White,
+                        dotSize = 14.dp,
+                        delayUnit = 300
+                    )
+                    Spacer(Modifier.size(10.dp))
+                    Text(
+                        text = "Loading",
+                        style = MaterialTheme.typography.h5.copy(
+                            color = Color.White,
+                            fontWeight = FontWeight.Medium
+                        )
+                    )
+                }
             }
         }
     }
@@ -97,12 +103,20 @@ fun UserListView(
 @Composable
 fun UserCard(
     imageUrl: String,
-    login: String
+    login: String,
 ) {
+    val animatedProgress = remember { Animatable(initialValue = 200f) }
+    LaunchedEffect(Unit) {
+        animatedProgress.animateTo(
+            targetValue = 0f,
+            animationSpec = tween(300, easing = FastOutSlowInEasing)
+        )
+    }
     Card(
         modifier = Modifier
             .fillMaxWidth()
-            .heightIn(min = 100.dp),
+            .heightIn(min = 100.dp)
+            .graphicsLayer(translationX = animatedProgress.value),
         backgroundColor = MaterialTheme.colors.background
     ) {
         Row(
@@ -111,12 +125,13 @@ fun UserCard(
                 .padding(10.dp),
             verticalAlignment = Alignment.CenterVertically
         ) {
+            val defaultImg = R.drawable.profile_img
             AsyncImage(
                 model = ImageRequest.Builder(LocalContext.current)
                     .data(imageUrl)
                     .crossfade(true)
                     .build(),
-                placeholder = painterResource(id = R.drawable.github_icon),
+                placeholder = painterResource(defaultImg),
                 contentDescription = "프로필 이미지",
                 contentScale = ContentScale.FillWidth,
                 modifier = Modifier
--- app/src/main/java/com/example/githubusersearch/framework/presentation/theme/Theme.kt
+++ app/src/main/java/com/example/githubusersearch/framework/presentation/theme/Theme.kt
@@ -1,9 +1,13 @@
 package com.example.githubusersearch.framework.presentation.theme
 
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
+import androidx.compose.material.ripple.RippleAlpha
+import androidx.compose.material.ripple.RippleTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
 private val MyPalette = darkColors(
@@ -44,4 +48,16 @@ fun GithubUserSearchTheme(
             darkIcons = darkSystemBar
         )
     }
+}
+
+object DarkRippleTheme: RippleTheme {
+
+    @Composable
+    override fun defaultColor(): Color = MaterialTheme.colors.primary
+
+    @Composable
+    override fun rippleAlpha(): RippleAlpha = RippleTheme.defaultRippleAlpha(
+        Color.Black,
+        lightTheme = !isSystemInDarkTheme()
+    )
 }
